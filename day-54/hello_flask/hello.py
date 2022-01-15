2264ba99590524ee9a6ddbccc133b3b2c163041a
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-15 10:58:31 +0200

    rendering html using flask

--- day-54/hello_flask/hello.py
+++ day-54/hello_flask/hello.py
@@ -3,7 +3,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+
 @app.route("/")
+@make_bold
+@make_underlined
+@make_emphasis
 def hello_world():
     return "Hello, World"
 
