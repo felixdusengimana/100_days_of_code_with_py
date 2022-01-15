c281bd973fba338a85d145a243b3a1dc26d4065e
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-15 10:44:36 +0200

    rendering html using flask

--- day-54/hello_flask/hello.py
+++ day-54/hello_flask/hello.py
@@ -18,5 +18,12 @@ def greet(name, age):
     return f"Hello there {name+ '12' } with years old {age+1}!"
 
 
+@app.route("/html")
+def dance():
+    return '<h1>Hello, world</h1>'\
+            '<img src="https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02' \
+           '/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1"/> '
+
+
 if __name__ == "__main__":
     app.run(debug=True)
