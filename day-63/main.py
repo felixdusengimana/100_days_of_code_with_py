86992d5def6a630b4b2d6f89aed4dd79dad96a56
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-20 09:50:42 +0200

    adding database

--- day-63/main.py
+++ day-63/main.py
@@ -1,4 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+# creating connection to a new database
+# db = sqlite3.connect("books-collection.db")
+
+# cursors for controlling our database
+# cursor = db.cursor()
+
+
 
 app = Flask(__name__)
 
@@ -7,14 +17,22 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(new_book)
+
+        return redirect(url_for('home'))
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
