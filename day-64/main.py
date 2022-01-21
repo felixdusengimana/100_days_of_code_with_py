c378785bfd120743f4a9d9118a34df8973e5ca8e
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-21 11:18:10 +0200

    Day 63 starting

---
+++ day-64/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+db = SQLAlchemy(app)
+
+
+class Movie(db.Model):
+    id
+    title
+    year
+    description
+    rating
+    ranking
+    review
+    img_url
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
