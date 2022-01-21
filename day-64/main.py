f8bfffb124039e421d4ff8211697591825c94e5f
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-21 11:38:45 +0200

    made my favorite movie app

--- day-64/main.py
+++ day-64/main.py
@@ -12,10 +12,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
 db = SQLAlchemy(app)
 
+MOVIE_DB_IMAGE_URL = ""
+MOVIE_DB_INFO_URL = ""
+MOVIE_DB_API_KEY = ""
+
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), nullable=False)
+    title = db.Column(db.String(250), nullable=False, unique=True)
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text(250), nullable=True)
     rating = db.Column(db.Float, nullable=True)
@@ -25,6 +29,55 @@ class Movie(db.Model):
 
 
 db.create_all()
+
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
+
+
+@app.route("/delete")
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        # The language parameter is optional, if you were making the website for a different audience
+        # e.g. Hindi speakers then you might choose "hi-IN"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            # The data in release_date includes month and day, we will want to get rid of.
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("home"))
 
 
 @app.route("/")
