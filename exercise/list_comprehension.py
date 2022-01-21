72d24ade91240e821087749686b8538418041f04
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-21 21:55:32 +0200

    donw with list comprehension

---
+++ exercise/list_comprehension.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+fruits_with_letter_a= []
+
+# for x in fruits:
+#   if "a" in x:
+#     fruits_with_letter_a.append(x)
+#
+# print(fruits_with_letter_a)
+
+name = "phelix"
+lettes_list = [letter for letter in name]
+print(lettes_list)
