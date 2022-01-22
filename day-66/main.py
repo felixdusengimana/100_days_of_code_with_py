c5e177bb52d869bdd52b5fd08731250a03a4ec3f
phelixdusengimana <phelixdusengimana@gmail.com>
2022-01-22 21:40:48 +0200

    Posting to a db

--- day-66/main.py
+++ day-66/main.py
@@ -2,6 +2,7 @@ import random
 
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import func
 
 app = Flask(__name__)
 
@@ -25,21 +26,82 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        # Method 1.
+        # dictionary = {}
+        # # Loop through each column in the data record
+        # for column in self.__table__.columns:
+        #     # Create a new dictionary entry;
+        #     # where the key is the name of the column
+        #     # and the value is the value of the column
+        #     dictionary[column.name] = getattr(self, column.name)
+        # return dictionary
+
+        # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
 
 # # HTTP GET - Read Record
 # get random record
 
 @app.route("/random")
 def get_random_cafe():
-    random_cafe = db.session.query(Cafe).all
-    return random.choice(random_cafe)
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+# get all
+
+@app.route("/all")
+def get_all():
+    cafes = db.session.query(Cafe).all()
+    cafe_dict = [cafe.to_dict() for cafe in cafes]
+    return jsonify(cafes=cafe_dict)
+
+
+# search
+@app.route("/search")
+def search_cafe():
+    query_location = request.args.get("loc").capitalize()
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={
+            "code": "404",
+            "message": "Sorry, we don't have a cafe at that location."
+        })
+
 
 # # HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def create_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={
+        "code": "200",
+        "status": "success",
+        "message": "Successfully added the new cafe."
+    })
+
 
 # # HTTP PUT/PATCH - Update Record
 
